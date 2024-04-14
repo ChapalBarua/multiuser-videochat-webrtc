eab95d960f10c2510c3d599c3453897e0a89deae
ChapalBarua <chapalbuet@yahoo.com>
2024-04-14 12:38:33 -0400

    [multiUserWebrtc-1] Fix code

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
-const io = require('socket.io')(server)
+const io = require('socket.io').listen(server)
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
